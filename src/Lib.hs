0446dc0472776b46c22c5fd07868842ae5aeea29
Curran McConnell <curran.mcconnell@protonmail.com>
2021-09-21 01:11:48 -0400

    format code

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,13 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Lib where
 
 -- strictness forces this list to be finite
 data FinList a
-  = a :| !(FinList a) 
+  = a :| !(FinList a)
   | Empty
   deriving stock (Show, Eq, Ord, Foldable)
 
@@ -14,11 +15,10 @@ instance Functor FinList where
   fmap _ Empty = Empty
   fmap f (a :| as) = f a :| fmap f as
 
-data Zipper a
-  = Zipper
-      { previous :: FinList a
-      , next :: FinList a
-      }
+data Zipper a = Zipper
+  { previous :: FinList a,
+    next :: FinList a
+  }
 
 mkZipper :: FinList a -> Zipper a
 mkZipper l = Zipper Empty l
@@ -33,28 +33,34 @@ right (Zipper as (b :| bs)) = Just $ Zipper (b :| as) bs
 
 fromZipper :: Zipper a -> FinList a
 fromZipper zipper =
- case left zipper of
-   Nothing -> next zipper
-   Just z -> fromZipper z
+  case left zipper of
+    Nothing -> next zipper
+    Just z -> fromZipper z
 
 -- we need terminal symbols of our language to be Concrete
 type Concrete a = (Eq a, Ord a, Bounded a, Enum a)
 
 -- Nonterminal symbols will be Abstract
+{- ORMOLU_DISABLE -}
 data Eq a => Abstract a
   = Start
   | Term a
   deriving stock (Show, Eq, Ord)
+{- ORMOLU_ENABLE -}
 
 -- Nothing represents the empty string, and is always a valid terminating symbol
+{- ORMOLU_DISABLE -}
 data (Concrete a, Ord b) => Alphabet a b
   = Terminal a
   | NonTerminal (Abstract b)
   deriving stock (Eq, Ord)
+{- ORMOLU_ENABLE -}
 
+{- ORMOLU_DISABLE -}
 data (Concrete a, Ord b) => Expression a b
   = Expression (FinList (Alphabet a b)) (Alphabet a b) (FinList (Alphabet a b))
   deriving stock (Eq, Ord)
+{- ORMOLU_ENABLE -}
 
 -- A relation may be expressed as a set of tuples (a, b)
 -- Rule is essentially a tuple, and Relation is roughly a set of tuples.
@@ -70,12 +76,13 @@ newtype Relation a b = Relation (FinList (Rule a b))
 
 -- these might want to be newtypes
 type Production a b = (Concrete a, Ord b) => Rule (Expression a b) (Expression a b)
+
 type Grammar a b = (Concrete a, Ord b) => Relation (Expression a b) (Expression a b)
 
 -- Start -> s
 unitGrammar :: (Eq a, Ord b) => Grammar a b
-unitGrammar = 
-  let relationElt =  Expression Empty (NonTerminal Start) Empty :-> Expression Empty (Terminal minBound) Empty
+unitGrammar =
+  let relationElt = Expression Empty (NonTerminal Start) Empty :-> Expression Empty (Terminal minBound) Empty
    in Relation $ relationElt :| Empty
 
 generate :: (Concrete a, Ord b) => FinList (Alphabet a b) -> Grammar a b -> Either (FinList (FinList ((Alphabet a b)))) (FinList (FinList a))
@@ -86,5 +93,5 @@ generate input grammar =
    in undefined
   where
     maxLength :: (Concrete a, Ord b) => (Expression a b -> FinList (Alphabet a b)) -> Grammar a b -> Int
-    maxLength lookDirection (Relation rules)
-      = maximum $ fmap (length . lookDirection . preimage) rules
+    maxLength lookDirection (Relation rules) =
+      maximum $ fmap (length . lookDirection . preimage) rules
