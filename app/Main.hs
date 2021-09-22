425cea9d8bde259ebfa3ff76a56639521f84c96f
Curran McConnell <curran.mcconnell@protonmail.com>
2021-09-22 18:54:17 -0400

    add app

---
+++ app/Main.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import Data.Int
+import Lib
+
+main :: IO ()
+main = do
+  let grammar = Grammar
+                  { grammarRules =
+		      ((0 :: Int16) :- (1 :: Int8)) :||
+		        Empty
+	          , grammarStart = 0
+		  , grammarEmptyString = 0
+		  }
+  putStrLn $ show $ generate grammar 4
