8e512c900342f7de4a9b3cd3f369db696441b645
Curran McConnell <curran.mcconnell@protonmail.com>
2021-09-21 22:36:40 -0400

    finish first attempt, get it redlighting

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,29 +5,54 @@
 
 module Lib where
 
+import Data.Either
 import Data.Foldable
+import Data.Functor
 import Data.Maybe
 
 -- strictness forces this list to be finite
 data FinList a
   = a :| !(FinList a)
   | Empty
-  deriving stock (Show, Eq, Ord, Foldable)
+  deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable)
+
+instance Semigroup (FinList a) where
+  (x :| xs) <> ys = x :| (xs <> ys)
+  Empty <> ys = ys
+
+instance Applicative FinList where
+  pure x = x :| Empty
+  fs@(f :| fss) <*> (x :| xs) = (f x :| (fs <*> xs)) <> (fss <*> xs)
+  _ <*> Empty = Empty
+  Empty <*> _ = Empty
 
 data NonNullFinList a
   = a :|| !(FinList a)
-  deriving stock (Show, Eq, Ord, Foldable)
+  deriving stock (Show, Eq, Ord, Functor, Foldable)
+
+fromNonNull :: NonNullFinList a -> FinList a
+fromNonNull (x :|| xs) = x :| xs
+
+instance Semigroup (NonNullFinList a) where
+  (x :|| xs) <> ys = x :|| (xs <> fromNonNull ys)
+
+instance Applicative NonNullFinList where
+  pure x = x :|| Empty
+  fs@(f :|| fss) <*> (x :|| xss) = f x :|| ((fromNonNull fs <*> xss) <> (fss <*> xss))
+
+prepend :: a -> NonNullFinList a -> NonNullFinList a
+prepend x (y :|| y' :| ys) = x :|| y :| y' :| ys
+prepend x (y :|| Empty) = x :|| y :| Empty
+
+grow :: NonNullFinList a -> FinList a -> NonNullFinList a
+grow (x :|| xs) ys = x :|| (xs <> ys)
 
 infixr 7 :|
 infixr 7 :||
 
-instance Functor FinList where
-  fmap _ Empty = Empty
-  fmap f (a :| as) = f a :| fmap f as
-
 data Zipper a = Zipper
   { previous :: !(FinList a),
-    next :: !(NonNullFinList a)
+    subsequent :: !(NonNullFinList a)
   }
   deriving (Show, Eq)
 
@@ -36,7 +61,7 @@ mkZipper l = Zipper Empty l
 
 left :: Zipper a -> Maybe (Zipper a)
 left (Zipper Empty _) = Nothing
-left (Zipper (a :| as) (b :|| bs)) = Just $ Zipper as (a :|| b :| bs)
+left (Zipper (a :| as) bs) = Just $ Zipper as (prepend a bs)
 
 right :: Zipper a -> Maybe (Zipper a)
 right (Zipper _ (_ :|| Empty)) = Nothing
@@ -45,7 +70,7 @@ right (Zipper as (b :|| b' :| bs)) = Just $ Zipper (b :| as) (b' :|| bs)
 fromZipper :: Zipper a -> NonNullFinList a
 fromZipper zipper =
   case left zipper of
-    Nothing -> next zipper
+    Nothing -> subsequent zipper
     Just z -> fromZipper z
 
 -- we need terminal symbols of our language to be Concrete
@@ -56,25 +81,117 @@ data (Ord nonterminal, Concrete terminal) => Rule nonterminal terminal
   | !nonterminal :- !terminal
   deriving (Show, Eq, Ord)
 
-data Grammar terminal nonterminal
+domain :: (Ord nonterminal, Concrete terminal) => Rule nonterminal terminal -> nonterminal
+domain (a := _) = a
+domain (a :- _) = a
+
+codomain :: (Ord nonterminal, Concrete terminal) => Rule nonterminal terminal -> Either (nonterminal, nonterminal) terminal
+codomain (_ := (a, b)) = Left (a, b)
+codomain (_ :- a) = Right a
+
+data Grammar nonterminal terminal
   = Grammar
-      { rules :: !(NonNullFinList (Rule nonterminal terminal))
-      , start :: !nonterminal
-      , emptyString :: !terminal
+      { grammarRules :: !(NonNullFinList (Rule nonterminal terminal))
+      , grammarStart :: !nonterminal
+      , grammarEmptyString :: !terminal
       }
   deriving (Show, Eq)
 
-generate :: (Concrete terminal, Ord nonterminal) => NonNullFinList (Either terminal nonterminal) -> Grammar terminal nonterminal -> NonNullFinList (NonNullFinList terminal)
-generate input rawGrammar =
+generate
+  :: (Concrete terminal, Ord nonterminal)
+  => Grammar nonterminal terminal
+  -> Int
+  -> FinList (NonNullFinList terminal)
+generate rawGrammar iterations =
+  -- we add the empty string to the language output if it is not already a rule
+  -- as per the definition of a context-free language
   let grammar =
-        if isJust $ find (== (start rawGrammar :- emptyString rawGrammar)) (rules rawGrammar)
+        if isJust $
+             find (== (grammarStart rawGrammar :- grammarEmptyString rawGrammar))
+             (grammarRules rawGrammar)
           then grammar
           else
-            let firstRule :|| otherRules = rules rawGrammar
-             in grammar
-                  { rules = (start rawGrammar :- emptyString rawGrammar) :|| (firstRule :| otherRules)
-                  }
-      input' = mkZipper input
-      rules' = mkZipper . rules $ grammar
-      innerIter zipState rulesState = undefined
-   in undefined
+            grammar
+              { grammarRules =
+                  (grammarStart rawGrammar :- grammarEmptyString rawGrammar)
+                    `prepend` grammarRules grammar
+              }
+   in generate'
+        (grammarRules grammar)
+        (pure $ pure (Left (grammarStart grammar)))
+        iterations
+
+generate'
+  :: (Concrete terminal, Ord nonterminal)
+  => NonNullFinList (Rule nonterminal terminal)
+  -> FinList (NonNullFinList (Either nonterminal terminal))
+  -> Int
+  -> FinList (NonNullFinList terminal)
+generate' _ Empty 0 = Empty
+generate' rules (generation :| generations) 0 =
+  if all isRight generation
+    then fmap (\(Right x) -> x) generation :| (generate' rules generations 0)
+    else generate' rules generations 0
+generate' rules generations iterations =
+  let mappedGenerations = generations <&> \generation ->
+        iterInput right (mkZipper generation) rules Empty
+      concatGenerations = foldl' (<>) Empty mappedGenerations
+   in generate' rules concatGenerations (iterations - 1)
+
+iterInput
+  :: (Concrete terminal, Ord nonterminal)
+  => (Zipper (Either nonterminal terminal) -> Maybe (Zipper (Either nonterminal terminal)))
+  -> Zipper (Either nonterminal terminal)
+  -> NonNullFinList (Rule nonterminal terminal)
+  -> FinList (NonNullFinList (Either nonterminal terminal))
+  -> FinList (NonNullFinList (Either nonterminal terminal))
+iterInput motion zipInput rules subsequentGeneration =
+  let substitutions = iterRules right zipInput (mkZipper rules) Empty
+      Zipper prev (_current :|| next) = zipInput
+      additionsToGeneration =
+        substitutions <&>
+          \substitution -> fromZipper $ Zipper prev $ substitution `grow` next
+   in case motion zipInput of
+        Nothing ->
+          subsequentGeneration
+        Just subsequentZipInput ->
+          iterInput
+            motion
+            subsequentZipInput
+            rules
+            (subsequentGeneration <> additionsToGeneration)
+
+iterRules
+  :: (Concrete terminal, Ord nonterminal)
+  => (Zipper (Rule nonterminal terminal) -> Maybe (Zipper (Rule nonterminal terminal)))
+  -> Zipper (Either nonterminal terminal)
+  -> Zipper (Rule nonterminal terminal)
+  -> FinList (NonNullFinList (Either nonterminal terminal))
+  -> FinList (NonNullFinList (Either nonterminal terminal))
+iterRules motion zipInput zipRules substitutions =
+  let Zipper _ (rule :|| _) = zipRules
+      Zipper _ (symbol :|| _) = zipInput
+      subsequentSubstitutions = prependRuleMatches rule symbol substitutions
+   in case motion zipRules of
+        Nothing ->
+          subsequentSubstitutions
+        Just subsequentZipRules ->
+          iterRules motion zipInput subsequentZipRules subsequentSubstitutions
+
+prependRuleMatches
+  :: (Ord nonterminal, Concrete terminal)
+  => Rule nonterminal terminal
+  -> Either nonterminal terminal
+  -> FinList (NonNullFinList ((Either nonterminal terminal)))
+  -> FinList (NonNullFinList (Either nonterminal terminal))
+prependRuleMatches rule symbol matches
+  = case symbol of
+      Left nonterminal ->
+        if domain rule == nonterminal
+          then case codomain rule of
+            Left (nonterminal1, nonterminal2) ->
+              ((Left nonterminal1 :|| Left nonterminal2 :| Empty)) :| matches
+            Right terminal ->
+              pure (Right terminal) :| matches
+          else matches
+      Right _ -> matches
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,8 @@
 module Main where
+
+import Data.Foldable
+import Data.Int
+import Data.List
 import Test.Hspec
 import Lib
 
@@ -13,3 +17,16 @@ main = hspec $ do
     it "converts back and forth" $
       (mkZipper . fromZipper . mkZipper $ ((1 :: Int) :|| 2 :| 3 :| Empty))
         `shouldBe` Zipper Empty ((1 :: Int) :|| 2 :| 3 :| Empty)
+  describe "generate" $ do
+    it "creates the empty output" $
+      let grammar =
+            Grammar
+              { grammarRules =
+                  ((0 :: Int16) := (1 :: Int16, 2 :: Int16)) :||
+                    (1 :- (3 :: Int8)) :|
+                    (2 :- 4) :|
+                    Empty
+              , grammarStart = 0
+              , grammarEmptyString = 0
+              }
+       in (sort . toList) (generate grammar 5) `shouldBe` [(0 :|| Empty), (3 :|| 4 :| Empty)]
